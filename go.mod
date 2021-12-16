b4dbfe5d50a454c2cf4e9b140535d78a84c9f226
David Mohl <git@davemail.io>
2021-12-16 15:21:49 +0900

    Init: Proof of concept

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/dvcrn/maskedemailcli
+
+go 1.16
+
+require github.com/mitchellh/mapstructure v1.4.3 // indirect
---
+++ responses.go
@@ -0,0 +1,35 @@
+package main
+
+/*
+{
+	"latestClientVersion": "267751068dba0064",
+	"methodResponses": [
+		["MaskedEmail/set", {
+			"newState": null,
+			"updated": {},
+			"created": {
+				"onepassword": {
+					"lastMessageAt": null,
+					"createdAt": "2021-12-16T05:46:02Z",
+					"state": "pending",
+					"id": "xxx",
+					"url": null,
+					"description": "",
+					"createdBy": "",
+					"email": "xxx@xxx.com"
+				}
+			},
+			"destroyed": [],
+			"accountId": "xxx",
+			"oldState": null
+		}, "0"]
+	],
+	"sessionState": "sessstate"
+}⏎
+*/
+
+// type GenericResponse struct {
+// 	LatestClientVersion string      `json:"latestClientVersion,omitempty"`
+// 	MethodResponses     interface{} `json:"methodResponses,omitempty"`
+// 	SessionState        string      `json:"sessionState,omitempty"`
+// }
