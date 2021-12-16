779ae8fac158a11023be7bbf13df2bd6a143e1b1
David Mohl <git@davemail.io>
2021-12-16 18:31:13 +0900

    Start moving api functions into client

---
+++ api.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/mitchellh/mapstructure"
+)
+
+const apiEndpoint = "https://api.fastmail.com/jmap/api/"
+
+type Client struct {
+	auth    string
+	accID   string
+	appName string
+}
+
+func NewClient(accID, token, appName string) *Client {
+	return &Client{
+		accID:   accID,
+		auth:    token,
+		appName: appName,
+	}
+}
+
+func (client *Client) sendRequest(r *APIRequest) (*APIResponse, error) {
+	reqJson, err := json.Marshal(r)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", apiEndpoint, bytes.NewBuffer(reqJson))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("authorization", fmt.Sprintf("Bearer %s", client.auth))
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	buf := &bytes.Buffer{}
+	_, err = buf.ReadFrom(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var apiRes APIResponse
+	err = json.Unmarshal(buf.Bytes(), &apiRes)
+	if err != nil {
+		return nil, err
+	}
+
+	return &apiRes, nil
+}
+
+func (client *Client) CreateMaskedEmail(forDomain string) (*MethodResponseCreateItem, error) {
+	mc := MethodCall{
+		MethodName: "MaskedEmail/set",
+		Payload:    NewMethodCallCreate(client.accID, client.appName, forDomain),
+		Payload2:   "0",
+	}
+
+	request := APIRequest{
+		Using: []string{
+			"urn:ietf:params:jmap:core",
+			"https://www.fastmail.com/dev/maskedemail",
+		},
+		MethodCalls: []MethodCall{mc},
+	}
+
+	res, err := client.sendRequest(&request)
+	if err != nil {
+		panic(err)
+	}
+
+	var pl MethodResponseCreate
+	err = mapstructure.Decode(res.MethodResponsesParsed[0].Payload, &pl)
+	if err != nil {
+		panic(err)
+	}
+
+	created, err := pl.GetCreatedItem()
+	if err != nil {
+		return nil, err
+	}
+
+	return &created, nil
+}
--- main.go
+++ main.go
@@ -6,10 +6,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"net/http"
 	"os"
-
-	"github.com/mitchellh/mapstructure"
 )
 
 // ["MaskedEmail/set", idkyet, 0]
@@ -56,9 +53,16 @@ func main() {
 	fmt.Println(*flagAppname)
 	// return
 
+	client := NewClient(*flagAccountID, *flagToken, *flagAppname)
+
+	res, err := client.CreateMaskedEmail(*flagDomain)
+	if err != nil {
+		panic(err)
+	}
+
 	r := MethodCall{
 		MethodName: "MaskedEmail/set",
-		Payload:    NewMethodCallCreate(*flagAccountID, *flagAppname, *flagDomain),
+		Payload:    NewMethodCallUpdate(*flagAccountID, res.ID),
 		Payload2:   "0",
 	}
 
@@ -70,55 +74,7 @@ func main() {
 		MethodCalls: []MethodCall{r},
 	}
 
-	reqJson, err := json.Marshal(cmer)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("first req")
-	// TODO: for debug. remove me.
-	func(v interface{}) {
-		j, err := json.MarshalIndent(v, "", "  ")
-		if err != nil {
-			fmt.Printf("%v\n", err)
-			return
-		}
-		buf := bytes.NewBuffer(j)
-		fmt.Printf("%v\n", buf.String())
-	}(cmer)
-
-	req, err := http.NewRequest("POST", apiEndpoint, bytes.NewBuffer(reqJson))
-	if err != nil {
-		panic(err)
-	}
-
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("authorization", fmt.Sprintf("Bearer %s", *flagToken))
-
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		panic(err)
-	}
-
-	buf := &bytes.Buffer{}
-	buf.ReadFrom(res.Body)
-
-	var genRe APIResponse
-	json.Unmarshal(buf.Bytes(), &genRe)
-
-	// TODO: for debug. remove me.
-	func(v interface{}) {
-		j, err := json.MarshalIndent(v, "", "  ")
-		if err != nil {
-			fmt.Printf("%v\n", err)
-			return
-		}
-		buf := bytes.NewBuffer(j)
-		fmt.Printf("%v\n", buf.String())
-	}(genRe.MethodResponsesParsed[0].Payload)
-
-	var pl MethodResponseCreate
-	err = mapstructure.Decode(genRe.MethodResponsesParsed[0].Payload, &pl)
+	sendRes, err := client.sendRequest(&cmer)
 	if err != nil {
 		panic(err)
 	}
@@ -132,60 +88,5 @@ func main() {
 		}
 		buf := bytes.NewBuffer(j)
 		fmt.Printf("%v\n", buf.String())
-	}(pl)
-
-	createdItem, err := pl.GetCreatedItem()
-	if err != nil {
-		panic(err)
-	}
-
-	r = MethodCall{
-		MethodName: "MaskedEmail/set",
-		Payload:    NewMethodCallUpdate(*flagAccountID, createdItem.ID),
-		Payload2:   "0",
-	}
-
-	cmer = APIRequest{
-		Using: []string{
-			"urn:ietf:params:jmap:core",
-			"https://www.fastmail.com/dev/maskedemail",
-		},
-		MethodCalls: []MethodCall{r},
-	}
-
-	fmt.Println("second res")
-	// TODO: for debug. remove me.
-	func(v interface{}) {
-		j, err := json.MarshalIndent(v, "", "  ")
-		if err != nil {
-			fmt.Printf("%v\n", err)
-			return
-		}
-		buf := bytes.NewBuffer(j)
-		fmt.Printf("%v\n", buf.String())
-	}(cmer)
-
-	reqJson, err = json.Marshal(cmer)
-	if err != nil {
-		panic(err)
-	}
-
-	req, err = http.NewRequest("POST", apiEndpoint, bytes.NewBuffer(reqJson))
-	if err != nil {
-		panic(err)
-	}
-
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("authorization", fmt.Sprintf("Bearer %s", *flagToken))
-
-	res, err = http.DefaultClient.Do(req)
-	if err != nil {
-		panic(err)
-	}
-
-	// TODO: for debug. remove me.
-	buf = &bytes.Buffer{}
-	buf.ReadFrom(res.Body)
-
-	fmt.Printf("%v", buf.String())
+	}(sendRes)
 }
